c54f949a8a397ba0eefc96afa760adb43a651f8d
Jaswinder Rattanpal <jrattanpal@gmail.com>
2021-03-03 16:46:16 -0500

    Render webinar info via JSON and use that

--- force-app/main/default/lwc/emailBuilderComponent/emailBuilderComponent.js
+++ force-app/main/default/lwc/emailBuilderComponent/emailBuilderComponent.js
@@ -1,4 +1,31 @@
 import { LightningElement, api } from 'lwc';
 export default class EmailBuilderComponent extends LightningElement {
     @api webinarList;
+    webinarListJson;
+    renderJson = false;
+    hasRendered = false;
+    msg = 'Please select a webinar';
+    
+    renderedCallback() {
+        if(this.hasRendered === false){
+            if(typeof(this.webinarList) != 'undefined'){
+                this.webinarListJson = JSON.parse(this.webinarList);
+                
+                if(typeof(this.webinarListJson.regUrl) != 'undefined'){
+                    //If true then show Webinar information
+                    this.renderJson = true;
+                    //we should mark this as rendred to avoid loop
+                    this.hasRendered = true;
+                }else{
+                    this.msg = this.webinarListJson.msg;
+                    console.log('this.webinarListJson: undefined' );
+                }
+            }else{
+                console.log('this.webinarList: undefined' );
+            }
+        }
+        //this.attachmentPoint = this.template.querySelector('div[innerhost=true]');
+        //this.attachmentPoint.innerHTML = this.webinarList;
+    }
+    /** */
 }
