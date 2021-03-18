584b4909bf7864b7c86eb936be69d9e2e9f25c79
Jaswinder Rattanpal <jrattanpal@gmail.com>
2021-03-18 14:02:35 -0400

    change code

--- force-app/main/default/lwc/emailBuilderComponent/emailBuilderComponent.js
+++ force-app/main/default/lwc/emailBuilderComponent/emailBuilderComponent.js
@@ -1,15 +1,6 @@
 import { LightningElement, api, wire } from 'lwc';
-//import getContactName from '@salesforce/apex/GoToWebinarController.getContactName';
 
 export default class EmailBuilderComponent extends LightningElement {
-    //@api contactFirstName;
-
-    //@api webinarList;
-    //webinarListJson;
-    //renderJson = false;
-    //hasRendered = false;
-    //msg = 'Please select a webinar';
-    @api webinarInfo;//delete this
     @api
     set htmlValue(value) {
         if (this.customHtml) {
