fb872c0541aa7460c458673b9cdc5d18446a087f
Jaswinder Rattanpal <jrattanpal@gmail.com>
2021-04-14 16:35:57 -0400

    change setter to include JSON processing

--- force-app/main/default/lwc/emailBuilderComponent/emailBuilderComponent.js
+++ force-app/main/default/lwc/emailBuilderComponent/emailBuilderComponent.js
@@ -6,14 +6,32 @@ export default class EmailBuilderComponent extends LightningElement {
     msg = 'Please select a webinar';
     webinarListJson;
 
-    @api htmlValue;
-    /*
+    @api 
     set htmlValue(value) {
         if (this.customHtml) {
             console.log('value::', value);
             this.customHtml.innerHTML = value;
         }
         this._htmlValue = value;
+        if(this.hasRendered === false){
+            if(typeof(this.htmlValue) != 'undefined'){
+                //var webinarList = '{' + this.webinarList + '}';
+                console.log(this.htmlValue);
+                this.webinarListJson = JSON.parse(this.htmlValue );
+                
+                if(typeof(this.webinarListJson.regUrl) != 'undefined'){
+                    //If true then show Webinar information
+                    this.renderJson = true;
+                    //we should mark this as rendred to avoid loop
+                    this.hasRendered = true;
+                }else{
+                    this.msg = this.webinarListJson.msg;
+                    console.log('this.webinarListJson: undefined' );
+                }
+            }else{
+                console.log('this.webinarList: undefined' );
+            }
+        }
     }
     get htmlValue() {
         return this._htmlValue;
