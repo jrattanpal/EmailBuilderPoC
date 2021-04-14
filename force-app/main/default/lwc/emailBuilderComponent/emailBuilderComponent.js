515cf5c8f39c9a664338ec1c9f61d713dab5a54a
Jaswinder Rattanpal <jrattanpal@gmail.com>
2021-04-14 16:18:21 -0400

    Testing JSON changes

--- force-app/main/default/lwc/emailBuilderComponent/emailBuilderComponent.js
+++ force-app/main/default/lwc/emailBuilderComponent/emailBuilderComponent.js
@@ -1,7 +1,13 @@
 import { LightningElement, api, wire } from 'lwc';
 
 export default class EmailBuilderComponent extends LightningElement {
-    @api
+    renderJson = false;
+    hasRendered = false;
+    msg = 'Please select a webinar';
+    webinarListJson;
+
+    @api htmlValue;
+    /*
     set htmlValue(value) {
         if (this.customHtml) {
             console.log('value::', value);
@@ -12,11 +18,31 @@ export default class EmailBuilderComponent extends LightningElement {
     get htmlValue() {
         return this._htmlValue;
     }
+    /**/
     
 
 
     renderedCallback() {
-        this.customHtml = this.template.querySelector('div[ishtmlcontainer=true]');
-        this.customHtml.innerHTML = this.htmlValue;
+        //this.customHtml = this.template.querySelector('div[ishtmlcontainer=true]');
+        //this.customHtml.innerHTML = this.htmlValue;
+
+        if(this.hasRendered === false){
+            if(typeof(this.htmlValue) != 'undefined'){
+                //var webinarList = '{' + this.webinarList + '}';
+                this.webinarListJson = JSON.parse(this.htmlValue );
+                
+                if(typeof(this.webinarListJson.regUrl) != 'undefined'){
+                    //If true then show Webinar information
+                    this.renderJson = true;
+                    //we should mark this as rendred to avoid loop
+                    this.hasRendered = true;
+                }else{
+                    this.msg = this.webinarListJson.msg;
+                    console.log('this.webinarListJson: undefined' );
+                }
+            }else{
+                console.log('this.webinarList: undefined' );
+            }
+        }
     }
 }
